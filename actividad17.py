b69d05e2e4274cc826eb40b16acade8e658955a9
sofidep08 <sx.dpaguirre@udeo.edu.gt>
2025-09-05 00:11:36 -0600

    cambios

--- actividad17.py
+++ actividad17.py
@@ -2,11 +2,14 @@ import tkinter as tk
 
 ventana = tk.Tk()
 ventana.title("Calculadora")
-ventana.geometry("600x600")
+ventana.geometry("600x500")
 ventana.configure(bg="light blue")
 
+fondo=tk.Frame(ventana,width=400, height=400, bg="pink")
+fondo.place(x=100, y=20)
+
 etiqueta1 = tk.Label(ventana, text="Ingrese el primer numero",font=("Bookman Old Style",14,"bold"), fg="turquoise")
-etiqueta1.pack(pady=4)
+etiqueta1.pack(pady=8)
 
 entrada1= tk.Entry(ventana)
 entrada1.pack(pady=4)
@@ -39,7 +42,7 @@ def multiplicar():
 def dividir():
     numero1=float(entrada1.get())
     numero2=float(entrada2.get())
-    if numero2== 0:
+    if numero2== 0 or numero1 == 0:
         etiqueta3.config(text="No se puede dividir entre 0",font=("Bookman Old Style",14,"bold"), fg="turquoise")
     else:
         etiqueta3.config(text=f"El resultado es: {numero1 / numero2}!", font=("Bookman Old Style", 14, "bold"), fg="turquoise")
@@ -60,7 +63,7 @@ boton_resta.pack(pady=4)
 boton_multiplicar=tk.Button(ventana,text="MULTIPLICAR *",command=multiplicar,font=("Bookman Old Style",12,"bold"),fg="turquoise",activebackground="pink")
 boton_multiplicar.pack(pady=4)
 
-boton_dividir=tk.Button(ventana,text="DIVIDIR *",command=multiplicar,font=("Bookman Old Style",12,"bold"),fg="turquoise",activebackground="pink")
+boton_dividir=tk.Button(ventana,text="DIVIDIR *",command=dividir,font=("Bookman Old Style",12,"bold"),fg="turquoise",activebackground="pink")
 boton_dividir.pack(pady=4)
 
 boton_limpiar = tk.Button(ventana, text="LIMPIAR", command=limpiar,font=("Bookman Old Style",12,"bold"),fg="turquoise",activebackground="pink")
