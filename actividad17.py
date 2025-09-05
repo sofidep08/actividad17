3e88db1d3165a5fb1bcb376bb996dd85fe4afdb6
sofidep08 <sx.dpaguirre@udeo.edu.gt>
2025-09-05 00:33:01 -0600

    cambios

--- actividad17.py
+++ actividad17.py
@@ -5,11 +5,11 @@ ventana.title("Calculadora")
 ventana.geometry("600x500")
 ventana.configure(bg="light blue")
 
-fondo=tk.Frame(ventana,width=400, height=400, bg="pink")
+fondo=tk.Frame(ventana,width=400, height=430, bg="pink")
 fondo.place(x=100, y=20)
 
 etiqueta1 = tk.Label(ventana, text="Ingrese el primer numero",font=("Bookman Old Style",14,"bold"), fg="turquoise")
-etiqueta1.pack(pady=8)
+etiqueta1.pack(pady=(70,4))
 
 entrada1= tk.Entry(ventana)
 entrada1.pack(pady=4)
@@ -54,22 +54,22 @@ def limpiar():
     entrada2.delete(0, tk.END)
     etiqueta3.config(text="Resultado",font=("Bookman Old Style",14,"bold"), fg="turquoise")
 
-boton_suma=tk.Button(ventana,text="SUMAR +",command=sumar,font=("Bookman Old Style",12,"bold"),fg="turquoise",activebackground="pink")
-boton_suma.pack(pady=4)
+boton_suma=tk.Button(ventana,text="SUMAR +",command=sumar,font=("Bookman Old Style",12,"bold"),fg="turquoise",activebackground="gray",relief="raised", bd=3)
+boton_suma.place(x=150,y=250)
 
-boton_resta=tk.Button(ventana,text="RESTAR -",command=restar,font=("Bookman Old Style",12,"bold"),fg="turquoise",activebackground="pink")
-boton_resta.pack(pady=4)
+boton_resta=tk.Button(ventana,text="RESTAR -",command=restar,font=("Bookman Old Style",12,"bold"),fg="turquoise",activebackground="gray",relief="raised", bd=3)
+boton_resta.place(x=350,y=250)
 
-boton_multiplicar=tk.Button(ventana,text="MULTIPLICAR *",command=multiplicar,font=("Bookman Old Style",12,"bold"),fg="turquoise",activebackground="pink")
-boton_multiplicar.pack(pady=4)
+boton_multiplicar=tk.Button(ventana,text="MULTIPLICAR *",command=multiplicar,font=("Bookman Old Style",12,"bold"),fg="turquoise",activebackground="gray",relief="raised", bd=3)
+boton_multiplicar.place(x=150,y=300)
 
-boton_dividir=tk.Button(ventana,text="DIVIDIR *",command=dividir,font=("Bookman Old Style",12,"bold"),fg="turquoise",activebackground="pink")
-boton_dividir.pack(pady=4)
+boton_dividir=tk.Button(ventana,text="DIVIDIR /",command=dividir,font=("Bookman Old Style",12,"bold"),fg="turquoise",activebackground="gray",relief="raised", bd=3)
+boton_dividir.place(x=350,y=300)
 
-boton_limpiar = tk.Button(ventana, text="LIMPIAR", command=limpiar,font=("Bookman Old Style",12,"bold"),fg="turquoise",activebackground="pink")
-boton_limpiar.pack(pady=4)
+boton_limpiar = tk.Button(ventana, text="LIMPIAR", command=limpiar,font=("Bookman Old Style",12,"bold"),fg="turquoise",activebackground="gray",relief="raised", bd=3)
+boton_limpiar.place(x=150,y=350)
 
-boton_salir = tk.Button(ventana, text="SALIR", command=ventana.quit,font=("Bookman Old Style",12,"bold"),fg="turquoise",activebackground="pink")
-boton_salir.pack(pady=4)
+boton_salir = tk.Button(ventana, text="SALIR", command=ventana.quit,font=("Bookman Old Style",12,"bold"),fg="turquoise",activebackground="gray",relief="raised", bd=3)
+boton_salir.place(x=350,y=350)
 
 ventana.mainloop()
